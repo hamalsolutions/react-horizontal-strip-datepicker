bd96dd566553ed640e46b68763946251a22775e2
Rafael Palma <exssrpalma@serviciosnutresa.onmicrosoft.com>
2021-09-15 17:43:31 -0400

    update also in dist

--- dist/ReactHorizontalDatePicker.js
+++ dist/ReactHorizontalDatePicker.js
@@ -89,7 +89,7 @@ export default function ReactHorizontalDatePicker({
 
   const onDateClick = day => {
     setSelectedDate(day);
-    selectedDay(selectedDate);
+    selectedDay(day);
   };
 
   const nextScroll = () => {
